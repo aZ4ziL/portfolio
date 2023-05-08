38fbe5d9394fe5f575cbb83a91ec0413c5778141
fajhri <vipercode305@gmail.com>
2023-05-08 16:32:39 +0700

    Redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isRedirect, setIsRedirect] = useState<boolean>(false);
+
+  if (isRedirect) {
+    redirect("/portfolio");
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsRedirect(true);
+    }, 2000);
+  });
+
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-[90%] md:w-[70%] lg:w-[50%]">
       <h1 className="text-3xl dark:text-white font-semibold uppercase">
         Selamat Datang Di Portfolio
       </h1>
